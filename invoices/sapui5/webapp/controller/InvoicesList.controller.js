b1a796725000c18aaf44bfe58ac942e27cd34f36
arielsa <santa.cruz.unla@gmail.com>
2024-11-17 21:51:01 +0000

    09_odata

--- invoices/sapui5/webapp/controller/InvoicesList.controller.js
+++ invoices/sapui5/webapp/controller/InvoicesList.controller.js
@@ -39,7 +39,7 @@ sap.ui.define(
                 };
                 const oList = this.byId("invoiceList");
                 const oBinding = oList.getBinding("items");
-                console.log(oBinding);
+                //console.log(oBinding);
                 oBinding.filter(aFilter);
             }
           }  
